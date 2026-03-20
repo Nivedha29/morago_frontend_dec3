d6c8bdd1f2ecd02cb8f4f08ed9082dbd442e5ca3
Juan Carlos Kerchaten <jcfiles07@gmail.com>
2026-03-20 14:36:15 +0900

    Style: added components to Tranlator Page

--- src/pages/admin/AdminTranslatorPage.jsx
+++ src/pages/admin/AdminTranslatorPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AdminHeader from "../../components/admin/AdminHeader";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import AdminPageShell from "../../components/admin/AdminPageShell";
-import "../../styles/AdminLayout.css"; // or your css file
+import "../../styles/AdminLayout.css";
 
 const AdminTranslatorPage = () => {
   return (
