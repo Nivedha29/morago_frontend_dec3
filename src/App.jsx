c30dfd32e5d8f4483fad9ad600bb99e07bd1b7f8
Juan Carlos Kerchaten <jcfiles07@gmail.com>
2026-03-23 12:39:52 +0900

    Added Initial Routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,15 @@ import { Routes, Route } from "react-router-dom";
 /* ------------------------- Admin Page ------------------------- */
 
 import AdminLoginPage from "./pages/admin/AdminLoginPage.jsx";
+import AdminForgotPasswordPage from "./pages/admin/AdminForgotPasswordPage.jsx";
+import AdminForgotPasswordVerifyPage from "./pages/admin/AdminForgotPasswordVerifyPage.jsx";
+import AdminForgotPasswordNewPasswordPage from "./pages/admin/AdminForgotPasswordNewPasswordPage.jsx";
 
-import AdminTranslatorPage from "./pages/admin/TranslatorPages/AdminTranslatorPage.jsx";
-import AddTranslatorPage from "./pages/admin/TranslatorPages/AddTranslatorPage.jsx";
-import TranslatorWithdrawHistoryPage from "./pages/admin/TranslatorPages/TranslatorWithdrawHistoryPage.jsx";
-import TranslatorWithdrawApproval from "./pages/admin/TranslatorPages/TranslatorWithdrawApproval.jsx";
-import TranslatorCallHistoryPage from "./pages/admin/TranslatorPages/TranslatorCallHistoryPage.jsx";
-
-import AdminUserPage from "./pages/admin/UsersPages/AdminUserPage.jsx";
-import AdminUserCallHistoryPage from "./pages/admin/UsersPages/UserCallHistoryPage.jsx";
-import UserDepositHistoryPage from "./pages/admin/UsersPages/UserDepositHistoryPage.jsx";
-import UserChargePage from "./pages/admin/UsersPages/UserChargePage.jsx";
-
+import AdminTranslatorPage from "./pages/admin/AdminTranslatorPage.jsx";
+import AddTranslatorPage from "./pages/admin/AddTranslatorPage.jsx";
+import AdminUserPage from "./pages/admin/AdminUserPage.jsx";
 import AdminThemesPage from "./pages/admin/AdminThemesPage.jsx";
 import AdminCategoriesPage from "./pages/admin/AdminCategoriesPage.jsx";
-import TranslatorWithdrawPage from "./pages/admin/TranslatorPages/TranslatorWithdrawPage.jsx";
 
 import SplashScreen from "./components/SplashScreen.jsx";
 import LoginScreen from "./pages/login.jsx";
@@ -36,47 +30,26 @@ import ForgotPasswordNewPasswordPage from "./pages/ForgotPasswordNewPasswordPage
 /* ------------------------- APP MAIN ------------------------- */
 const App = () => {
   return (
-    /* ------------------------- ADMIN ------------------------- */
     <Routes>
       <Route path="/admin/login" element={<AdminLoginPage />} />
-
+      <Route
+        path="/admin/forgot-password"
+        element={<AdminForgotPasswordPage />}
+      />
+      <Route
+        path="/admin/forgot-password/verify"
+        element={<AdminForgotPasswordVerifyPage />}
+      />
+      <Route
+        path="/admin/forgot-password/new-password"
+        element={<AdminForgotPasswordNewPasswordPage />}
+      />
+      
       <Route path="/admin/translators" element={<AdminTranslatorPage />} />
       <Route path="/admin/translators/add" element={<AddTranslatorPage />} />
-      <Route
-        path="/admin/translators/:translatorId/withdraw"
-        element={<TranslatorWithdrawPage />}
-      />
-      <Route
-        path="/admin/translators/:translatorId/withdraw-history"
-        element={<TranslatorWithdrawHistoryPage />}
-      />
-      <Route
-        path="/admin/translators/:translatorId/withdraw/:withdrawalId/approval"
-        element={<TranslatorWithdrawApproval />}
-      />
-      <Route
-        path="/admin/translators/:id/call-history"
-        element={<TranslatorCallHistoryPage />}
-      />
-
       <Route path="/admin/users" element={<AdminUserPage />} />
-      <Route
-        path="/admin/users/:userId/call-history"
-        element={<AdminUserCallHistoryPage />}
-      />
-      <Route
-        path="/admin/users/:userId/deposit-history"
-        element={<UserDepositHistoryPage />}
-      />
-      <Route
-        path="/admin/users/:userId/charge/:depositId"
-        element={<UserChargePage />}
-      />
-
       <Route path="/admin/themes" element={<AdminThemesPage />} />
       <Route path="/admin/categories" element={<AdminCategoriesPage />} />
-
-      {/* ------------------------- MOBILE APP ------------------------- */}
 
       <Route
         path="*"
