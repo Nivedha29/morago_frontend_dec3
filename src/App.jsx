04d37679252f733019c9204b01ce95b6cc9cc99b
Juan Carlos Kerchaten <jcfiles07@gmail.com>
2026-03-24 03:36:34 +0900

    Admin: Refactor AdminTable actions and add routing for Call and Withdraw History pages

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,10 @@ const App = () => {
         path="/admin/translators/:translatorId/withdraw-history/:withdrawalId/approval"
         element={<TranslatorWithdrawApproval />}
       />
+      <Route
+        path="/admin/translators/:id/call-history"
+        element={<TranslatorCallHistoryPage />}
+      />
       <Route path="/admin/users" element={<AdminUserPage />} />
       <Route path="/admin/themes" element={<AdminThemesPage />} />
       <Route path="/admin/categories" element={<AdminCategoriesPage />} />
