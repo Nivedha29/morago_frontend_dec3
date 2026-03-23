c977c3930ba05ca66c3254cc73b00dc123850d48
Juan Carlos Kerchaten <jcfiles07@gmail.com>
2026-03-24 03:46:21 +0900

    created API to fetch list of Users

---
+++ src/services/adminUser.ts
@@ -0,0 +1,51 @@
+import api from "./api";
+
+///////////////////////////////////////////////////////////
+// USERS
+///////////////////////////////////////////////////////////
+
+export interface UserItem {
+  id: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  balance: number;
+  hasDepositRequest: boolean;
+}
+
+export interface UserListResponse {
+  totalElements: number;
+  totalPages: number;
+  number: number;
+  size: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+  content: UserItem[];
+}
+
+export interface GetAdminUsersParams {
+  keyword?: string;
+  isDebtor?: boolean;
+  hasDeposit?: boolean;
+  page?: number;
+  size?: number;
+  sortBy?: string;
+  sortDirection?: "ASC" | "DESC";
+}
+
+export const getAdminUsers = async (
+  params: GetAdminUsersParams = {},
+): Promise<UserListResponse> => {
+  const response = await api.get("/admin/users", {
+    params: {
+      page: 0,
+      size: 5,
+      sortBy: "id",
+      sortDirection: "ASC",
+      ...params,
+    },
+  });
+
+  return response.data;
+};
